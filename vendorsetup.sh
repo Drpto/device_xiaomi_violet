832a59c5ae2f8f8830d58b96df0da4af52f705ea
Drpto <dr.juniornoob@gmailcom>
2024-08-03 13:00:56 +0530

    violet: track firmware & kernel from my git
    Signed-off-by: Drpto <dr.juniornoob@gmailcom>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,13 @@
 # vendor & Kernel stuff
 git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet.git -b fourteen vendor/xiaomi/violet 
-git clone https://github.com/Joker-V2/kernel_xiaomi_violet.git -b fourteen kernel/xiaomi/violet
+git clone https://github.com/Drpto/kernel_xiaomi_violet.git -b 14 kernel/xiaomi/violet
 
 #kernel clang
 rm -rf prebuilts/clang/host/linux-x86/clang-r498229
 git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git prebuilts/clang/host/linux-x86/clang-r498229
 
 #Firmware
-git clone https://gitlab.com/Joker-V2/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
+git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
 
 # Device clangs stuff
 rm -rf packages/resources/devicesettings
